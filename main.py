5ac86eb94cd371a16a621c5f901e52b32f534bbb
talleeenos69 <talleeenos69@gmail.com>
2023-03-03 10:44:20 -0500

    Commit 8

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/my-flask-route', methods=['POST'])
-def handle_form_submission():
-    data = request.form.get('data')
-    # do something with the submitted data
-    return 'Form submitted successfully!'
